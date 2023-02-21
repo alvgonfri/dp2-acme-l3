ecdf2f415ad5d18a471146e0d7c73767f3e4e4b9
Alvaro Gonzalez <alvgonfri@alum.us.es>
2023-02-21 17:45:04 +0100

    Banner entity created closes #39

---
+++ Acme-L3-D02/src/main/java/acme/entities/banners/Banner.java
@@ -0,0 +1,54 @@
+
+package acme.entities.banners;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Banner extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	protected Date				moment;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				displayStart;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				displayEnd;
+
+	@URL
+	protected String			picture;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			slogan;
+
+	@URL
+	protected String			target;
+
+	// Attributes -------------------------------------------------------------
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
