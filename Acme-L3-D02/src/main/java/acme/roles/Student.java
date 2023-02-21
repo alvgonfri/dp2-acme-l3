7db06f02554a63cb2de1a632d17eef21d13c1ee2
Alvaro Gonzalez <alvgonfri@alum.us.es>
2023-02-22 00:06:39 +0100

    Student role created

---
+++ Acme-L3-D02/src/main/java/acme/roles/Student.java
@@ -0,0 +1,44 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Student extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			statement;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			strongFeatures;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			weakFeatures;
+
+	@URL
+	protected String			moreInfo;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
