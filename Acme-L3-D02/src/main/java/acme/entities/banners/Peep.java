2bbe9b1bc5a1f8caea88662366582d58424df38f
carlosdelrioperez <carlosmix52@gmail.com>
2023-02-23 13:48:59 +0100

    Create Peep.java
    Entity peep

---
+++ Acme-L3-D02/src/main/java/acme/entities/banners/Peep.java
@@ -0,0 +1,55 @@
+
+package acme.entities.banners;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Peep extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date				moment;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			nick;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			message;
+
+	@Email
+	protected String			email;
+
+	@URL
+	protected String			link;
+
+}
