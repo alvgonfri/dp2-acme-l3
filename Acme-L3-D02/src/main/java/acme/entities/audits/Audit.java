8b5b2ad471eb74d45454f48c0074cdd9b1ee5fdc
Miguel Manzano Álvarez <73229701+miguelma1@users.noreply.github.com>
2023-03-02 14:24:05 +0100

    feat: primera versión entidad Audit
    faltan relaciones y revision final

---
+++ Acme-L3-D02/src/main/java/acme/entities/audits/Audit.java
@@ -0,0 +1,54 @@
+
+package acme.entities.audits;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Audit extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	@NotBlank
+	@Column(unique = true)
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			conclusion;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			strongPoints;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			weakPoints;
+
+	// Derived attributes -----------------------------------------------------
+
+
+	@NotNull
+	public Mark getMark() {
+		return Mark.A;
+	}
+	// Relationships ----------------------------------------------------------
+
+	//FALTAN RELACIONES
+}
