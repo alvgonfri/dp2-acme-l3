53b4f75e589622dba2ff8bc254c452ae75d0a971
Miguel Manzano Álvarez <73229701+miguelma1@users.noreply.github.com>
2023-03-14 20:51:14 +0100

    fix: poblado de audit

--- Acme-L3-D02/src/main/java/acme/entities/audits/Audit.java
+++ Acme-L3-D02/src/main/java/acme/entities/audits/Audit.java
@@ -1,23 +1,17 @@
 
 package acme.entities.audits;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
-import org.springframework.data.util.Pair;
 
 import acme.entities.courses.Course;
 import acme.framework.data.AbstractEntity;
+import acme.roles.Auditor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -52,33 +46,15 @@ public class Audit extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 
-	@NotNull
-	public Mark getMark() {
-		final List<Pair<Mark, Integer>> aux = new ArrayList<>();
-		final Mark[] marks = Mark.values();
-		for (final Mark m : marks) {
-			final Pair<Mark, Integer> p = Pair.of(m, this.auditingRecords.stream().filter(x -> x.getMark() == m).collect(Collectors.toList()).size());
-			aux.add(p);
-		}
-
-		aux.stream().sorted((x, y) -> y.getSecond().compareTo(x.getSecond())).collect(Collectors.toList());
-
-		//si solo hay una nota que es moda la devuelvo
-		if (aux.get(0).getSecond() != aux.get(1).getSecond())
-			return aux.get(0).getFirst();
-		else {
-			final Integer mode = aux.get(0).getSecond();
-			return aux.stream().filter(x -> x.getSecond() == mode).findAny().get().getFirst();
-
-		}
+	public void getMark() {
 
 	}
 	// Relationships ----------------------------------------------------------
 
 
 	@ManyToOne
-	protected Course				course;
+	protected Course	course;
 
-	@OneToMany
-	protected List<AuditingRecord>	auditingRecords;
+	@ManyToOne
+	protected Auditor	auditor;
 }
