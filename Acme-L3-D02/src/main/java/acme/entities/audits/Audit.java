863d773cf0ee2cc5f9501ecc14cf44cfc0ce4abb
Miguel Manzano Álvarez <73229701+miguelma1@users.noreply.github.com>
2023-03-15 17:24:03 +0100

    fix: version final Audit
    La relacion con Auditor dejara de dar error cuando se inlcuya el rol

--- Acme-L3-D02/src/main/java/acme/entities/audits/Audit.java
+++ Acme-L3-D02/src/main/java/acme/entities/audits/Audit.java
@@ -1,20 +1,13 @@
 
 package acme.entities.audits;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
-import org.springframework.data.util.Pair;
 
 import acme.entities.courses.Course;
 import acme.framework.data.AbstractEntity;
@@ -32,7 +25,7 @@ public class Audit extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 
-	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	@Pattern(regexp = "[A-Z]{1,3}\\d{3}")
 	@NotBlank
 	@Column(unique = true)
 	protected String			code;
@@ -51,34 +44,11 @@ public class Audit extends AbstractEntity {
 
 	// Derived attributes -----------------------------------------------------
 
-
-	@NotNull
-	public Mark getMark() {
-		final List<Pair<Mark, Integer>> aux = new ArrayList<>();
-		final Mark[] marks = Mark.values();
-		for (final Mark m : marks) {
-			final Pair<Mark, Integer> p = Pair.of(m, this.auditingRecords.stream().filter(x -> x.getMark() == m).collect(Collectors.toList()).size());
-			aux.add(p);
-		}
-
-		aux.stream().sorted((x, y) -> y.getSecond().compareTo(x.getSecond())).collect(Collectors.toList());
-
-		//si solo hay una nota que es moda la devuelvo
-		if (aux.get(0).getSecond() != aux.get(1).getSecond())
-			return aux.get(0).getFirst();
-		else {
-			final Integer mode = aux.get(0).getSecond();
-			return aux.stream().filter(x -> x.getSecond() == mode).findAny().get().getFirst();
-
-		}
-
-	}
 	// Relationships ----------------------------------------------------------
 
+	@ManyToOne
+	protected Course			course;
 
 	@ManyToOne
-	protected Course				course;
-
-	@OneToMany
-	protected List<AuditingRecord>	auditingRecords;
+	protected Auditor			auditor;
 }
