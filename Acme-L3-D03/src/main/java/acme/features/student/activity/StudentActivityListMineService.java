c77db8539aea435d8ebdb41f3c914ba98b75731f
Alvaro Gonzalez <alvgonfri@alum.us.es>
2023-03-23 20:11:44 +0100

    List and show activities

---
+++ Acme-L3-D03/src/main/java/acme/features/student/activity/StudentActivityListMineService.java
@@ -0,0 +1,58 @@
+
+package acme.features.student.activity;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.enrolments.Activity;
+import acme.framework.components.accounts.Principal;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+import acme.roles.Student;
+
+@Service
+public class StudentActivityListMineService extends AbstractService<Student, Activity> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected StudentActivityRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Activity> objects;
+		Principal principal;
+
+		principal = super.getRequest().getPrincipal();
+		objects = this.repository.findActivitiesByStudentId(principal.getActiveRoleId());
+
+		super.getBuffer().setData(objects);
+	}
+
+	@Override
+	public void unbind(final Activity object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "enrolment.code", "title", "type", "startDate", "endDate");
+
+		super.getResponse().setData(tuple);
+	}
+
+}
