2c5636b055fbf25ec38f5572c3c5614fa408be17
Miguel Manzano Álvarez <73229701+miguelma1@users.noreply.github.com>
2023-04-18 15:25:02 +0200

    fix: ahora muestra solo los banners disponibles

--- Acme-L3-D03/src/main/java/acme/components/BannerRepository.java
+++ Acme-L3-D03/src/main/java/acme/components/BannerRepository.java
@@ -1,10 +1,10 @@
 
 package acme.components;
 
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.Query;
 
 import acme.entities.banners.Banner;
@@ -12,29 +12,26 @@ import acme.framework.repositories.AbstractRepository;
 
 public interface BannerRepository extends AbstractRepository {
 
-	@Query("select count(b) from Banner b")
-	int countBanners();
+	@Query("select count(b) from Banner b where displayStart<= :date AND displayEnd>= :date")
+	int countAvailableBanners(Date date);
 
-	@Query("select b from Banner b")
-	List<Banner> findManyBanners(PageRequest pageRequest);
+	@Query("select b from Banner b where displayStart<= :date AND displayEnd>= :date")
+	List<Banner> findManyAvailableBanners(Date date);
 
 	default Banner findRandomBanner() {
 		Banner result;
 		int count, index;
 		ThreadLocalRandom random;
-		PageRequest page;
 		List<Banner> list;
 
-		count = this.countBanners();
+		count = this.countAvailableBanners(new Date());
 		if (count == 0)
 			result = null;
 		else {
 			random = ThreadLocalRandom.current();
 			index = random.nextInt(0, count);
-
-			page = PageRequest.of(index, 1);
-			list = this.findManyBanners(page);
-			result = list.isEmpty() ? null : list.get(0);
+			list = this.findManyAvailableBanners(new Date());
+			result = list.isEmpty() ? null : list.get(index);
 		}
 
 		return result;
