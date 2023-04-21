45ba6e181cf3d47e14eec9455a3c679776c372f1
Miguel Manzano Álvarez <73229701+miguelma1@users.noreply.github.com>
2023-04-21 02:15:49 +0200

    fix: correccion validaciones update

--- Acme-L3-D03/src/main/java/acme/features/administrator/offer/AdministratorOfferUpdateService.java
+++ Acme-L3-D03/src/main/java/acme/features/administrator/offer/AdministratorOfferUpdateService.java
@@ -2,6 +2,8 @@
 package acme.features.administrator.offer;
 
 import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -67,6 +69,31 @@ public class AdministratorOfferUpdateService extends AbstractService<Administrat
 	public void validate(final Offer object) {
 		assert object != null;
 
+		Calendar calendar;
+		final Date latestDate;
+
+		calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 2100);
+		calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+		calendar.set(Calendar.DAY_OF_MONTH, 31);
+		calendar.set(Calendar.HOUR_OF_DAY, 23);
+		calendar.set(Calendar.MINUTE, 59);
+		calendar.set(Calendar.SECOND, 59);
+		calendar.set(Calendar.MILLISECOND, 0);
+		latestDate = new Date(calendar.getTimeInMillis());
+
+		final Date earliestDate;
+
+		calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 2000);
+		calendar.set(Calendar.MONTH, Calendar.JANUARY);
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		earliestDate = new Date(calendar.getTimeInMillis());
+
 		if (!super.getBuffer().getErrors().hasErrors("moment") && !super.getBuffer().getErrors().hasErrors("startAvailable")) {
 			boolean isValid;
 
@@ -78,10 +105,27 @@ public class AdministratorOfferUpdateService extends AbstractService<Administrat
 		if (!super.getBuffer().getErrors().hasErrors("endAvailable") && !super.getBuffer().getErrors().hasErrors("startAvailable")) {
 			boolean isValid;
 
-			isValid = MomentHelper.isLongEnough(object.getStartAvailable(), object.getEndAvailable(), 1, ChronoUnit.WEEKS);
+			isValid = MomentHelper.isLongEnough(object.getStartAvailable(), object.getEndAvailable(), 1, ChronoUnit.WEEKS) && MomentHelper.isBefore(object.getStartAvailable(), object.getEndAvailable());
 
 			super.state(isValid, "startAvailable", "administrator.offer.error.oneweeklong");
 			super.state(isValid, "endAvailable", "administrator.offer.error.oneweeklong");
+
+		}
+
+		if (!super.getBuffer().getErrors().hasErrors("startAvailable")) {
+
+			boolean isValid;
+
+			isValid = MomentHelper.isBefore(object.getStartAvailable(), latestDate);
+			super.state(isValid, "startAvailable", "administrator.offer.error.datelimits");
+		}
+
+		if (!super.getBuffer().getErrors().hasErrors("endAvailable")) {
+
+			boolean isValid;
+
+			isValid = MomentHelper.isAfter(object.getEndAvailable(), earliestDate) && MomentHelper.isBefore(object.getEndAvailable(), latestDate);
+			super.state(isValid, "endAvailable", "administrator.offer.error.datelimits");
 		}
 	}
 
